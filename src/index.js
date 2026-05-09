98fb927bae033fb16f14837f4a04932b8a04e159
SpBrother20 <1849472698@qq.com>
2026-05-09 08:23:12 +0800

    改为react应用

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-
-// 页面容器
-const app = document.createElement("div");
-app.id = "app-view";
-document.body.appendChild(app);
 import demoImg from "./img/demo.jpg";
 
-function renderHome() {
-  app.innerHTML = `
-    <h1>你好，世界~</h1>
-    <button id="btn">点我</button>
-    <div style="margin: 20px 0">
-      <button id="goto-page1">跳转到 Page1</button>
-    </div>
+function Home({ goToPage1 }) {
+  return (
     <div>
-      <img src="${demoImg}" alt="" />
+      <h1>你好，世界~</h1>
+      <button
+        onClick={() => {
+          alert("按钮被点击");
+          console.log("clicked");
+        }}
+      >
+        点我
+      </button>
+      <div style={{ margin: "20px 0" }}>
+        <button onClick={goToPage1}>跳转到 Page1</button>
+      </div>
+      <div>
+        <img src={demoImg} alt="" />
+      </div>
     </div>
-  `;
-  document.getElementById("btn").onclick = () => {
-    alert("按钮被点击");
-    console.log("clicked");
-  };
-  document.getElementById("goto-page1").onclick = renderPage1;
+  );
 }
 
-function renderPage1() {
-  app.innerHTML = `
-    <h1>页面1</h1>
-    <h2>Page 1 JS 加载成功</h2>
-    <button id="back-home">返回首页</button>
-  `;
-  document.getElementById("back-home").onclick = renderHome;
+function Page1({ goHome }) {
+  return (
+    <div>
+      <h1>页面1</h1>
+      <h2>Page 1 JS 加载成功</h2>
+      <button onClick={goHome}>返回首页</button>
+    </div>
+  );
 }
 
-// 默认渲染首页
-renderHome();
+function App() {
+  const [page, setPage] = useState("home");
+  return page === "home" ? (
+    <Home goToPage1={() => setPage("page1")} />
+  ) : (
+    <Page1 goHome={() => setPage("home")} />
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
